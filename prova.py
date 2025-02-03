e505ce8b57ac33fa99649c42a751d68c525b2852
cri2725 <cristian.istrati27@gmail.com>
2025-02-03 15:06:05 +0100

    prima modifica primo file

--- prova.py
+++ prova.py
@@ -1,2 +1,17 @@
-print("Hello World!")
-print("Hello World!")
+def somma(a,b):
+    """
+    somma due numeri 
+    argo:a e b
+    ret: somma
+    """
+    return a+b
+
+var = 2
+var2 = 4
+for i in range(10):
+    var+= 3
+    var2+=5
+somma=somma(var,var2)
+print("num: {}".format(var))
+print("num: {}".format(var2))
+print("la somma è {}".format(somma))
